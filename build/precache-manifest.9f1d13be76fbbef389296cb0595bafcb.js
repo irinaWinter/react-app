24e6fc16e3ac3ee8d88b269e1d14ad67ebcc8edd
Itina Winter <lobanova.irina.96@mail.ru>
2020-07-30 15:30:21 +0300

    Оформляет компоненты

---
+++ build/precache-manifest.9f1d13be76fbbef389296cb0595bafcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59dc250bd5860ebe5512ee7145f40c12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16ff0a00aec6ab001fcf",
+    "url": "/static/css/2.b07ff35c.chunk.css"
+  },
+  {
+    "revision": "e8a6521d357b35d61243",
+    "url": "/static/css/main.a0096691.chunk.css"
+  },
+  {
+    "revision": "16ff0a00aec6ab001fcf",
+    "url": "/static/js/2.1a5f618d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a5f618d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a6521d357b35d61243",
+    "url": "/static/js/main.eca02334.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
---
+++ src/components/Article.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+class Article extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			isOpen: props.defaultOpen
+		}
+	}
+
+	render () {
+		const {article} = this.props;	  
+	  const body = this.state.isOpen && <section className='card-text'>{article.text}</section>
+	  const style = {width: '50%'}
+
+		return (
+	    <div  className='card mx-auto' style = {style}>
+	    	<div className='card-header'>
+		    	<h2>
+		    		{article.title}
+		    		<button onClick={this.handleClick} className='btn btn-primary btn-lg float-right'>
+		    			{this.state.isOpen ? 'close' : 'open'}
+		    		</button>
+		    	</h2>
+		    </div>	
+		    <div className='card-body'>
+		      {body}
+		      <h6 className='card-subtitle text-muted'>creation date: {(new Date(article.date)).toDateString()}</h6>  
+		    </div>  
+	    </div>  
+	  );
+	}
+
+	handleClick = () => {
+		console.log('---', 'clicked');
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	}
+};
+
+
+export default Article;
