ae09f7f49ad8552d3bcf801286f73843b71280ef
Itina Winter <lobanova.irina.96@mail.ru>
2020-08-01 11:47:53 +0300

    Отлаживает приложение

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import ArticleList from './ArticleList';
 import articles from '../fixtures';
 import 'bootstrap/dist/css/bootstrap.css';
 
-class App extends Component {
+class App extends PureComponent {
 	state = {
 		reverted: false
 	}
 
 	render () {
+		console.log(1)
 		return (
 	    <div className='container'>
 	    	<div className='bg-primary'>
@@ -17,14 +18,16 @@ class App extends Component {
 	    			<button className='btn' onClick = {this.revert}>Revert</button>
 	    		</h1>
 	    	</div>
-	      <ArticleList articles = {this.state.reverted ? articles.reverse() : articles}/>
+	      <ArticleList articles = {this.state.reverted ? articles.slice().reverse() : articles}/>
 	    </div>
 	  )    
   }
 
-  revert = () => this.setState({
-  	reverted: !this.state.reverted
-  })
+  revert = () => {
+	  this.setState({
+	  	reverted: !this.state.reverted
+	  })
+	}  
 };
 
 export default App;
--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Article from '../Article'
 import './style.css'
 
-export default function ArticleList ({ articles }) {
-	const articleElements = articles.map((article, index) => 
-		<li key = {article.id} className='article-list__li'>
-			<Article article = {article} defaultOpen = {index === 0} />
-		</li>
-	)
+export default class ArticleList extends Component {
+	render () {
+		console.log(2)
+		const articleElements = this.props.articles.map((article, index) =>
+			<li key = {article.id} className='article-list__li'>
+				<Article article = {article} defaultOpen = {index === 0} />
+			</li>
+		)
 
-	return (
-		<ul>
-			{articleElements};
-		</ul>
-	);
+		return (
+			<ul>
+				{articleElements}
+			</ul>
+		)
+	}
 }
